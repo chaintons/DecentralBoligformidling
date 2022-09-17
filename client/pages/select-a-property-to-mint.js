46bc866e41b175891e882dea88cc1f1c16aead80
binordev <binordev@protonmail.com>
2022-09-17 02:53:00 +0200

    continue sign button

--- client/pages/select-a-property-to-mint.js
+++ client/pages/select-a-property-to-mint.js
@@ -25,7 +25,17 @@ export default function SelectProperty() {
                     disabled={!formInput.address}>
                     Select Property
                 </button>
-                <div className="pt-4">{propertyOwner}</div>
+                <div 
+                    className="pt-4"
+                    hidden={!propertyOwner}>
+                    Are you {propertyOwner} the owner of {formInput.address}?
+                    <button 
+                        // onClick={selectPropertyOnWeb2} 
+                        className="font-bold mt-4, ml-4 bg-teal-400 text-white rounded p-4 shadow-lg"
+                        disabled={!formInput.address}>
+                        Yes, continue to sign proofs
+                    </button>
+                </div>
             </div>
         </>
     )
