39674fd328b6304e07583e7307923cf901bd065d
binordev <binordev@protonmail.com>
2022-09-17 02:23:24 +0200

    addr added to req

--- client/pages/api/mocks/select-property/[address].js
+++ client/pages/api/mocks/select-property/[address].js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ owner: 'John Doer' })
+  const { address } = req.query
+  res.status(200).json({ owner: `John Doer`, address: address})
 }
--- client/pages/select-a-property-to-mint.js
+++ client/pages/select-a-property-to-mint.js
@@ -1,18 +1,31 @@
+import { useState } from 'react'
+
 export default function SelectProperty() {
+    const [propertyOwner, setPropertyOwner] = useState('')
+    const [formInput, updateFormInput] = useState({ address: '' })
+
     async function selectPropertyOnWeb2() {
-        const res = await fetch('api/mocks/select-property')
+        const res = await fetch(`api/mocks/select-property/${formInput.address}`)
         const json = await res.json()
+        setPropertyOwner(json.owner)
         console.log(JSON.stringify(json.owner))
     }
 
     return (
         <>
-            <div className="flex justify-center">
-                <div className="w-1/2 flex flex-col pb-12">
-                    <button onClick={selectPropertyOnWeb2} className="font-bold mt-4 bg-teal-400 text-white rounded p-4 shadow-lg">
-                        Select Property
-                    </button>
-                </div>
+            <div className="card p-4">
+                <input 
+                    placeholder="Address"
+                    className="mt-8 border rounded p-4"
+                    onChange={e => updateFormInput({ ...formInput, address: e.target.value })}
+                />
+                <button 
+                    onClick={selectPropertyOnWeb2} 
+                    className="font-bold mt-4, ml-4 bg-teal-400 text-white rounded p-4 shadow-lg"
+                    disabled={!formInput.address}>
+                    Select Property
+                </button>
+                <div className="pt-4">{propertyOwner}</div>
             </div>
         </>
     )
