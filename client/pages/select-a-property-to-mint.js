302a0915dc767fee6ab56ea76c558dd83f89ee6f
binordev <binordev@protonmail.com>
2022-09-17 19:43:30 +0200

    signCertificateWithWallet added

--- client/pages/select-a-property-to-mint.js
+++ client/pages/select-a-property-to-mint.js
@@ -1,9 +1,18 @@
 import { useState } from 'react'
 import { getWeb3Async } from '../utils/web3utils'
+import { keccak256 } from 'web3-utils'
 
 export default function SelectProperty() {
     const [propertyOwner, setPropertyOwner] = useState('')
+    const [certificateToSign, setCertificateToSign] = useState('')
     const [formInput, updateFormInput] = useState({ address: '' })
+    const [myEthAccount, setMyEthAccount] = useState('')
+    const [dataToSignHash, setDataToSignHash] = useState('')
+
+    function isDataToSignHashValid() {
+        const isValid = (dataToSignHash != undefined && dataToSignHash != null && dataToSignHash.length>6)
+        return isValid
+    }
 
     function onChangeAddress(e) {
         updateFormInput({ ...formInput, address: e.target.value })
@@ -20,19 +29,35 @@ export default function SelectProperty() {
     async function prepareCertificate(propertyOwner) {
         const web3 = await getWeb3Async()
         const accounts = await web3.eth.getAccounts()
-        const myEthAccount = accounts[0]
+        setMyEthAccount(accounts[0])
         
         if (myEthAccount.length === 42) {
             console.log('myEthAddr ' + myEthAccount)
 
-            const certificateToSign = `I, ${propertyOwner}, have signed this certificate 
-            with my NemId and my ETH wallet address: ${myEthAccount}`
+            setCertificateToSign(`I, ${propertyOwner}, have signed this certificate 
+            with my NemId and my ETH wallet address: ${myEthAccount}`)
 
-            // Example of sign with ethers:
-            // https://github.com/MetaMask/test-dapp/blob/main/src/index.js#L879
-            // Which to choose? https://ethereum.stackexchange.com/a/25610
-            //web3.eth.sign(dataToSign, myEthAccount , callback)
+            //console.log('certificateToSign ' + certificateToSign)
+            setDataToSignHash(await keccak256(certificateToSign))
+            console.log('dataToSignHash ' + dataToSignHash)
         }
+    }
+
+    function signCertificateWithWalletCb(error, walletSignature) {
+        if (error) {
+            console.log('Error with signCertificateWithWallet ' + error)
+            return
+        }
+        console.log('walletSignature ' + walletSignature)
+    }
+
+    async function signCertificateWithWallet() {
+        const web3 = await getWeb3Async()
+
+        // Example of sign with ethers:
+        // https://github.com/MetaMask/test-dapp/blob/main/src/index.js#L879
+        // Which to choose? https://ethereum.stackexchange.com/a/25610
+        web3.eth.sign(dataToSignHash, myEthAccount , signCertificateWithWalletCb)
     }
 
     return (
@@ -52,12 +77,33 @@ export default function SelectProperty() {
                 <div 
                     className="pt-4"
                     hidden={!propertyOwner}>
-                    Are you {propertyOwner} the owner of {formInput.address}?
+                    <span>
+                        Are you {propertyOwner} the owner of {formInput.address}?
+                    </span>
                     <button 
                         onClick={e => prepareCertificate(propertyOwner)} 
                         className="font-bold mt-4, ml-4 bg-teal-400 text-white rounded p-4 shadow-lg"
                         disabled={!formInput.address}>
                         Yes, continue to sign proofs
+                    </button>
+                </div>
+                <div 
+                    className="pt-4"
+                    hidden={!propertyOwner||!certificateToSign||!isDataToSignHashValid()}>
+                    <div>Proove that you own wallet address {myEthAccount} by singing following certificate:</div>
+                    <br></br>
+                    <div>
+                        <i>{certificateToSign}</i>
+                    </div>
+                    <button 
+                        onClick={e => signCertificateWithWallet()} 
+                        className="font-bold mt-4, ml-4 bg-teal-400 text-white rounded p-4 shadow-lg">
+                        Sign cetificate with you wallet
+                    </button>
+                    <button 
+                        // onClick={e => bbbb(propertyOwner)} 
+                        className="font-bold mt-4, ml-4 bg-teal-400 text-white rounded p-4 shadow-lg">
+                        Sign cetificate with MitId
                     </button>
                 </div>
             </div>
