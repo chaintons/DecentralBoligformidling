642e2bc42a7e222b96cba90b37f1eca4843b6b59
binordev <binordev@protonmail.com>
2022-09-18 09:37:00 +0200

    fix merge

--- client/pages/index.js
+++ client/pages/index.js
@@ -35,8 +35,6 @@ export default function Home() {
           owner: i.buyer,
           image: meta.data.image,
           name: meta.data.name,
-          tokenURI: tokenURI,
-          propertyAddress: meta.data.propertyAddress,
           description: meta.data.description
         }
         return nft
